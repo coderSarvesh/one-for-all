c6b2ffa9fcf40d763e3ca72e37d78f989273de8b
Sarvesh Rane <ranesarvesh80@gmail.com>
2023-07-04 18:34:11 +0530

    backend user auth mechanism ready

--- routes/auth.js
+++ routes/auth.js
@@ -1,32 +1,107 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
-const {body , validationResult} = require('express-validator');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-router.post('/createuser',[
-    body('name').isLength({min:3}), //express validator used for cleaning inputs before saving in db
-    body('email',"Enter Valid email").isEmail(),
-    body('password').isLength({min:5})
-] ,async (req,res) => {
+const JWT_SECRET = process.env.JWT_SECRET;
+var fetchuser = require('../middleware/fetchUser');
+
+//create user
+router.post('/createuser', [
+    body('name').isLength({ min: 3 }), //express validator used for cleaning inputs before saving in db
+    body('email', "Enter Valid email").isEmail(),
+    body('password').isLength({ min: 5 })
+], async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
     }
 
-    let user = await User.findOne({email:req.body.email});
-    if (user) {
-        return res.status(400).json("uSER already exists");
-    }
-    user = await User.create({
+    try {
+        let user = await User.findOne({ email: req.body.email }); // checking if user is already present
+        if (user) {
+            return res.status(400).json("USER already exists");
+        }
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password,salt);
+
+        user = await User.create({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
+            password: secPass,
         })
-    
-    .then(user=>res.json(user))
-    .catch(error => {console.log(error) 
-        res.json("Please enter a unique email")})
+        const data ={
+            user:{
+                id:user.id
+            }
+        }
+        const authToken = jwt.sign(data,JWT_SECRET);
+        // console.log(jwtdata)
+        res.json({authToken});
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal server error");
+    }
 })
 
+// Authenticate user
+router.post('/login', [ //express validator used for cleaning inputs before saving in db
+    body('email', "Enter Valid email").isEmail(),
+    body('password', "Cannot be blank").exists()
+] , async (req,res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {email,password} = req.body;
+    try {
+        let user = await User.findOne({email});
+        if(!user){
+            return res.status(400).json({error:"Internal Server error"});
+        }
+
+    const passwordCompare = await bcrypt.compare(password,user.password);
+    if(!passwordCompare){
+        return res.status(400).json({error:"Try with correct credentials"});
+    }
+    const data ={
+        user:{
+            id:user.id
+        }
+    }
+    const authToken = jwt.sign(data,JWT_SECRET);
+    res.json({authToken});
+    } 
+    
+    catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal server error");
+    }
+
+})
+
+//user details
+router.post('/getuser',fetchuser ,async (req,res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        userId = req.user.id;
+        const user = await User.findById(userId).select("-password");
+        res.send(user); 
+    } 
+    
+    catch (error) {
+        console.log(error.message);
+        res.status(500).send("Internal server error");
+    }
+
+})
 
 module.exports = router;
